56f75ed575067250dffbe8d68ee033823476b7ca
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-10-26 14:41:06 +0300

    Create basicOperations.kt

---
+++ MiniNotes/basicOperations.kt
@@ -0,0 +1,43 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    
+    //Değişkenler
+    
+    val isim = "wokdlkw"
+    val sotisi = "ksndkn"
+    val sapas = isim + " " + sotisi 
+  
+    
+    println(sapas)
+    
+    
+    var a = 5
+    var b = 10
+    
+    var yas = 60
+
+    println ( yas / b * a )
+    var number : String
+    number = "23"
+    
+    var number2 = "45"
+    println(number + number2)
+
+    //int variable
+	var benimNumaram : Int = 20
+    var yeniNumaram = 10
+    
+    println(yeniNumaram / 2)
+	
+    
+    //Long variable
+    
+    var benimLong : Long = 50000000000000
+    println(benimLong)
+    
+    var 
+
+}
