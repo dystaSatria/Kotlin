ea7a3db4b50fe73972457820c4cf9748ec0eb8f7
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-10-28 23:53:28 +0300

    Create oopBasic.kt

---
+++ MiniNotes/oopBasic.kt
@@ -0,0 +1,23 @@
+class Food(val name: String, val taste: String, val brand: String, val price: Double) {
+    
+    fun displayInfo() {
+        println("Food: $name")
+        println("Taste: $taste")
+        println("Brand: $brand")
+        println("Price: $price")
+    }
+}
+
+fun main() {
+    // Create some Food objects
+    val food1 = Food("Fried Rice", "Savory", "Simple Eats", 15.0)
+    val food2 = Food("Chicken Satay", "Sweet and Spicy", "Sate Garden", 20.0)
+    val food3 = Food("Spaghetti","Barbeque")
+
+    // Display food information
+    println("Food 1 Information:")
+    food1.displayInfo()
+
+    println("\nFood 2 Information:")
+    food2.displayInfo()
+}
